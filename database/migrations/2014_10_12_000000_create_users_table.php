1d6ef1c5061dcb2fa5a5d5c4405186f3d972a1a5
Louis Haans <louis.hans@viacesi.fr>
2018-07-02 21:23:23 +0200

    try

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,18 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
- 
+            $table->integer('roles_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
 
 
         Schema::table('users', function (Blueprint $table) {
-            $table->UnsignedInteger('roles_id');
             $table->foreign('roles_id')
             ->references('id')
             ->on('roles')
