a605571c60107ad77b1e24a4e630f3915e393475
Louis Haans <louis.hans@viacesi.fr>
2018-07-02 21:56:54 +0200

    try

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,18 +19,17 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->unsignedInteger('roles_id');
-            $table->foreign('roles_id')
-            ->references('id')
-            ->on('roles')
-            ->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
 
 
-        // Schema::table('users', function (Blueprint $table) {
-   
-        // });
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('roles_id')
+            ->references('id')
+            ->on('roles')
+            ->onDelete('cascade');
+        });
     }
 
     /**
